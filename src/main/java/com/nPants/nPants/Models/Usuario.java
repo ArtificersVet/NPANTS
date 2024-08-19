cbc7aaf2b114b6b40ee523c64d7954edff919b35
Emerson1911 <1284662@clases.edu.sv>
2024-08-19 11:46:45 -0600

    User terminado

--- src/main/java/com/nPants/nPants/Models/Usuario.java
+++ src/main/java/com/nPants/nPants/Models/Usuario.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 
 @Entity
@@ -20,7 +21,7 @@ public class Usuario {
 
     private String password;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "rol_id")
     private Rol rolid;
 
