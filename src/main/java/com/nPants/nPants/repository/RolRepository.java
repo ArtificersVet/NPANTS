f591a27d59d022d5a76084330925085719e32659
Emerson1911 <1284662@clases.edu.sv>
2024-08-21 09:59:10 -0600

    Paginacion en rol y busqueda

--- src/main/java/com/nPants/nPants/repository/RolRepository.java
+++ src/main/java/com/nPants/nPants/repository/RolRepository.java
@@ -1,9 +1,12 @@
 package com.nPants.nPants.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nPants.nPants.Models.Rol;
 
 public interface RolRepository extends JpaRepository<Rol, Long> {
-
+    // Método para buscar roles por nombre con paginación
+    Page<Rol> findByNombreContaining(String nombre, Pageable pageable);
 }
