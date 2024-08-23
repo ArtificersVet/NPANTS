fcdb33f4b32ee3547829ec37be236c15d6ee5f69
IGabriel30 <ingridgabriel30@hotmail.com>
2024-08-22 20:36:26 -0600

    Buscador y paginación en Talla

--- src/main/java/com/nPants/nPants/Controllers/TallaController.java
+++ src/main/java/com/nPants/nPants/Controllers/TallaController.java
@@ -1,6 +1,7 @@
 package com.nPants.nPants.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;  
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.nPants.nPants.Models.Talla;
 import com.nPants.nPants.services.*; 
@@ -22,8 +23,22 @@ public class TallaController {
     private TallaServices tallaService; 
 
     @GetMapping()
-    public String listarTallas(Model model) {
-        model.addAttribute("tallas", tallaService.listarTodas()); 
+    public String listarTodas(@RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "5") int size,
+        @RequestParam(defaultValue = "") String nombre,Model model) {
+
+         Page<Talla> tallas;
+        if (nombre.isEmpty()) {
+            tallas = tallaService.listarTodas(page, size);
+        } else {
+            tallas = tallaService.buscarPorNombre(nombre, page, size);
+        }
+        model.addAttribute("tallas", tallas.getContent()); 
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", tallas.getTotalPages());
+        model.addAttribute("totalItems", tallas.getTotalElements());
+        model.addAttribute("pageSize", size);
+        model.addAttribute("nombre", nombre);
         return "tallas/tallas-list"; 
     }
 
